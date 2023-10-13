b72bc9c33533f28e1babd7895013f2064a5ca95f
DurgaSowmya <durgasowmyakollipara131203@gmail.com>
2023-10-13 20:35:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const wordCount = text.trim().split(/\s+/).length;
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <textarea
+        rows="5"
+        cols="50"
+        placeholder="Type something..."
+        value={text}
+        onChange={handleChange}
+      />
+      <p>Word Count: {wordCount}</p>
     </div>
   );
 }
